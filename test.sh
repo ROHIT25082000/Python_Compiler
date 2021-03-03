94a9294ca3fa761a87599dbddd46241ca955bec5
Rohit Vishwakarma <52505486+ROHIT25082000@users.noreply.github.com>
2021-03-03 23:42:02 +0530

    Add files via upload
    The grammar is added and also a test script

---
+++ test.sh
@@ -0,0 +1,104 @@
+echo "------------------------------------------------------"
+cat < input1.txt
+echo "Test <1>"
+echo "This is a test on input1.txt"
+sleep 3
+./python.out < input1.txt
+
+echo "Test Successful on input1.txt..[*]"
+sleep 5
+clear
+
+echo "------------------------------------------------------"
+cat < input2.txt
+echo "Test <2>"
+echo "This is a test on input2.txt"
+sleep 3
+./python.out < input2.txt
+
+echo "Test Successful on input2.txt..[*]"
+sleep 5
+clear
+
+echo "-----------------------------------------------------"
+cat < input3.txt
+echo "Test <3>"
+echo "This is a test on input3.txt"
+sleep 3
+./python.out < input3.txt
+
+echo "Test Successful on input3.txt..[*]"
+sleep 5
+clear
+
+
+echo "-----------------------------------------------------"
+
+cat < input4.txt
+
+echo "Test <4>"
+echo "This is a test on input.txt"
+sleep 3
+./python.out < input4.txt
+
+echo "Test Successful on input4.txt..[*]"
+sleep 5
+clear
+
+
+echo "-----------------------------------------------------"
+cat < input5.txt
+echo "Test <5>"
+echo "This is a test on input5.txt"
+sleep 3
+./python.out < input5.txt
+
+echo "Test Successful on input5.txt..[*]"
+sleep 5
+clear
+
+echo "-----------------------------------------------------"
+cat < input6.txt
+echo "Test <6>"
+echo "This is a test on input6.txt"
+sleep 3
+./python.out < input6.txt
+
+echo "Test Successful on input6.txt..[*]"
+sleep 5
+clear
+
+echo "-----------------------------------------------------"
+cat < input7.txt
+echo "Test <7>"
+echo "This is a test on input7.txt"
+sleep 3
+./python.out < input7.txt
+
+echo "Test Successful on input7.txt..[*]"
+sleep 5
+clear
+
+echo "------------------------------------------------------"
+cat < input8.txt
+echo "Test <8>"
+echo "This is a test on input8.txt"
+sleep 3
+./python.out < input8.txt
+
+echo "Test Successful on input8.txt..[*]"
+sleep 5
+clear
+
+echo "-----------------------------------------------------"
+cat < input.txt
+
+echo "Test <9>"
+echo "This is a test on input.txt"
+sleep 3
+./python.out < input.txt
+
+echo "Test Successful on input.txt ...[*]"
+
+
+echo "All test passed .............."
